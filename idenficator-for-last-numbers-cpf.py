6867a73fb19ac4d7c70197745154b4251089796f
FLIRER4547 <fernandomarianosilva613@gmail.com>
2025-07-07 20:39:57 -0300

    cpf-generator-for-educational-tests

--- idenficator-for-last-numbers-cpf.py
+++ idenficator-for-last-numbers-cpf.py
@@ -14,4 +14,4 @@ if i1>9:
 i2=((n1*11+n2*10+n3*9+n4*8+n5*7+n6*6+n7*5+n8*4+n9*3+i1*2)*10)%11
 if i2>9:
         i2=0
-print(' o CPF completo é: ',n1,n2,n3,n4,n5,n6,n7,n8,n9,i1,i2)
+print(f'O CPF completo é: \033[1;40m{n1}{n2}{n3}.{n4}{n5}{n6}.{n7}{n8}{n9}-{i1}{i2}\033[0m')
